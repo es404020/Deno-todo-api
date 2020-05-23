cd02d94ec100c313f1a68455f655dc114432e1d6
ajani eniola <eniolsolomon@gmail.com>
2020-05-23 15:32:45 +0100

    auth user sign in and generate jwt

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 import { App, Request, Response,logger,parser } from "https://deno.land/x/attain/mod.ts";
 import { init, MongoClient } from "https://deno.land/x/mongo@v0.6.0/mod.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
+
+
+import { validateJwt } from "https://deno.land/x/djwt/validate.ts";
+import {
+    makeJwt,
+    setExpiration,
+    Jose,
+    Payload,
+} from "https://deno.land/x/djwt/create.ts";
+
 await init();
 const client = new MongoClient();
 client.connectWithUri(
@@ -10,6 +20,7 @@ const db = client.database('test');
 const todo= db.collection('todos');
 const users= db.collection('users');
 const app = new App();
+const key="denos";
 app.use(logger);
 app.use(parser);
 
@@ -146,6 +157,71 @@ try{
 
 
   })
+
+
+  app.post("/auth",async (req,res)=>{
+   
+ try{
+
+  const user:any = await validate(req);
+
+  const payload:Payload={
+    iss:user._id.$oid,
+  }
+
+  const header:Jose={
+    alg:"HS256",
+    typ:"JWT"
+  }
+
+const token = makeJwt({header,payload,key});
+
+res.status(200).send({
+token:token,
+_id:user._id.$oid
+});
+
+
+
+
+ }
+catch(err){
+
+res.status(400).send(err);
+
+}
+
+  })
+
+
+  function validate(req:any){
+    const {email, password} = req.params;
+
+
+
+    return new Promise(async (resolve,reject)=>{
+
+try{
+
+  const user = await users.findOne({email:email});
+
+  const pwdValidate = bcrypt.checkpw(password,user.password);
+ 
+  if(pwdValidate){
+    resolve(user);
+  }
+  else{
+    reject('Authentication failed.')
+  }
+  
+
+}catch(err){
+  reject('Authentication failed.')
+}
+
+
+    })
+  }
 
 app.listen({ port: 3500 });
 
